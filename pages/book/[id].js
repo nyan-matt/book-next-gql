e5a5064f961ea5eaae24d81735a4c7c3226327f1
matt <matt.rea@gmail.com>
2020-06-23 20:30:45 -0500

    formatting...

--- pages/book/[id].js
+++ pages/book/[id].js
@@ -62,68 +62,77 @@ function Book({ data }) {
 
   if (router.isFallback) {
     return (
-      <div styles={{postion: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}}>
-            Loading...
+      <div
+        styles={{
+          postion: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        Loading...
       </div>
     );
   }
-  
+
   return (
     <div className="lg:pt-32 sm:pt-16 lg:pb-40 sm:pb-2">
-        {book ? (
-        <motion.div initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} className="container mx-auto px-6 md:flex">  
+      {book ? (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          className="container mx-auto px-6 md:flex"
+        >
           <div className="w-full md:w-1/2">
-            <img className="w-full rounded border border-color-gray-500" src={book.coverArt ? book.coverArt : "/no-image.png"}
+            <img
+              className="w-full rounded border border-color-gray-500"
+              src={book.coverArt ? book.coverArt : "/no-image.png"}
               alt={`Cover art - ${book.title}`}
             />
-            
           </div>
           <div className="w-full md:w-1/2 md:ml-4 sm:ml-0 md:mt-0 sm:mt-4 ">
-            <h1 className="text-default font-bold text-4xl">
-              {book.title}
-              </h1>
+            <h1 className="text-default font-bold text-4xl">{book.title}</h1>
             <h2 className="text-default font-thin text-2xl -mt-2">
               {book.subTitle}
-              </h2>
+            </h2>
             <div className="text-default mt-8">
-                {book.authors.map((author, index) => (
-                  <span key={index}>
-                    {author.name}
-                    {book.authors.length > 1 && index < book.authors.length - 1
-                      ? ", "
-                      : " "}
-                  </span>
-                ))}
-              </div>
-              <div className="text-default">
-                {book.rating && (
-                  <StarRatingComponent
-                    name={book._id}
-                    starCount={5}
-                    value={book.rating}
-                    editing={false}
-                    className="text-2xl"
-                  />
-                )}
-              </div>
-              <div className="text-default">
-                {book.tags.map((tag, index) => (
-                  <span 
-                    key={index}
-                    className="text-xs bg-transparent rounded mr-2 text-primary px-2 py-1 border border-primary"
-                  >
-                    {tag.name}
-                  </span>
-                ))}
-              </div>
-              <div className="text-default mt-8 blockquote">
-                {book.comments}
-              </div>
+              {book.authors.map((author, index) => (
+                <span key={index}>
+                  {author.name}
+                  {book.authors.length > 1 && index < book.authors.length - 1
+                    ? ", "
+                    : " "}
+                </span>
+              ))}
+            </div>
+            <div className="text-default">
+              {book.rating && (
+                <StarRatingComponent
+                  name={book._id}
+                  starCount={5}
+                  value={book.rating}
+                  editing={false}
+                  className="text-2xl"
+                />
+              )}
+            </div>
+            <div className="text-default">
+              {book.tags.map((tag, index) => (
+                <span
+                  key={index}
+                  className="text-xs bg-transparent rounded mr-2 text-primary px-2 py-1 border border-primary"
+                >
+                  {tag.name}
+                </span>
+              ))}
+            </div>
+            <div className="text-default mt-8 blockquote">{book.comments}</div>
           </div>
         </motion.div>
-        ) : (
-          <h1>Ooops, couldn't find that book. </h1>
-        )}
+      ) : (
+        <h1>Ooops, couldn't find that book. </h1>
+      )}
     </div>
   );
 }
